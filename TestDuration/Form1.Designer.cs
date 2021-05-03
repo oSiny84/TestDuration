86214b96c2d27c1b2005846cb0e079df69a03275
unknown <ever43@gmail.com>
2021-05-03 14:20:03 +0900

    Init version

---
+++ TestDuration/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace TestDuration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(458, 351);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TestDuration.Properties.Resources.Gage_Pin;
+            this.pictureBox2.Location = new System.Drawing.Point(323, 75);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(11, 77);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Down";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TestDuration.Properties.Resources.Gage;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 279);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 351);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TestDuration/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+
+namespace TestDuration
+{
+    public partial class Form1 : Form
+    {
+        int baseX = 0, baseY = 0, picW, picH;
+        int value = 0;
+        System.Windows.Forms.Timer timerSec = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            picW = pictureBox1.Size.Width;
+            picH = pictureBox1.Size.Height;
+            timerSec.Interval = 10;
+            timerSec.Tick += new EventHandler(timerSec_Tick);
+            timerSec.Start();
+        }
+
+        void timerSec_Tick(object sender, EventArgs e)
+        {
+            this.pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics grfx = e.Graphics;
+            DrawAnalogClock(grfx);
+        }
+
+        private void DrawAnalogClock(Graphics g)
+        {
+            int center = baseX + picH / 2;
+            //int centerY = baseY + picW / 2;
+
+            double valueAngle = 2 * Math.PI * (value+45/*Center Angle*/) / 100;
+
+            
+            //double radian = valueAngle * (Math.PI / 180);
+
+            g.DrawLine(new Pen(Brushes.Red, 7), center, center,
+                center + (int)(100 * Math.Cos(valueAngle)),
+                center + (int)(100 * Math.Sin(valueAngle)));
+
+
+            Image img = Image.FromFile("Gage_Pin.png");
+
+            
+            Matrix _mat = new Matrix();
+            _mat.Rotate(value);
+
+            g.Transform = _mat;
+            g.DrawImage(img, center,center);
+
+
+
+            label1.Text = "VAL:" + value + "    Angle:" + valueAngle + "   Rad:";// +radian;
+            label2.Text = " SIN:" + ((int)(100 * Math.Sin(valueAngle))) + "    COS:" + ((int)(100 * Math.Cos(valueAngle)));
+            label2.Text += " X:" + (center + (int)(100 * Math.Sin(valueAngle))) + " Y:" + (center + (int)(100 * Math.Cos(valueAngle)));
+        }
+
+
+        private void PlusThread()
+        {
+            for (int i = 0; i <= 60; i++)
+            {
+                Thread.Sleep(20);
+                value = i;
+            }
+        }
+        private void MinusThread()
+        {
+            for (int i = 60; i >= 0; i--)
+            {
+                Thread.Sleep(20);
+                value = i;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(new ThreadStart(PlusThread));
+            a.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(new ThreadStart(MinusThread));
+            a.Start();
+        }
+    }
+}
